b646a4910a942a820f5fe49b5a8e3a87b3ae5d7f
Muhammad Umar <muhammadumar@exceedgulf.com>
2025-07-22 18:10:31 +0500

    git update

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
             targets: ["MainSDK"]),
     ],
     targets: [
-        .binaryTarget(
-            name: "InnerFramework",
-            url: "https://github.com/mumariossys/InnterFramework/releases/download/1.0/InnerFramework.xcframework.zip",
-             checksum: "1f8eea42dc49cf9c3e692bda74e8418413b95db44a9b68a7a1673f114d67b06b"),
+//        .binaryTarget(
+//            name: "InnerFramework",
+//            url: "https://github.com/mumariossys/InnterFramework/releases/download/1.0/InnerFramework.xcframework.zip",
+//             checksum: "1f8eea42dc49cf9c3e692bda74e8418413b95db44a9b68a7a1673f114d67b06b"),
         .binaryTarget(
             name: "MainSDK",
-            url: "https://github.com/mumariossys/MainSDK/releases/download/1.0/MainSDK.xcframework.zip",
-             checksum: "955eab2582ce5bfeda7ba7a60dab40fbfeb5676e285e9a244552810599e6c0e6")
+            url: "https://github.com/mumariossys/MainSDK/releases/download/1.0.0/MainSDK.xcframework.zip",
+             checksum: "3360515b597461d613345d1a642fa9c1b12d9bab6ea9164a693538a45da3a315")
     ]
 )
